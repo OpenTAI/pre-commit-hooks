18f9c699e32d5d425feb2c2cb73bb05fca0d33d0
jamesxwang <jameswangx95@gmail.com>
2022-02-16 15:32:41 +0800

    feat: init say hello hook

--- README.md
+++ README.md
@@ -2,3 +2,20 @@
 
 Some pre-commit hooks for OpenTAI projects.
 
+## Using pre-commit-hooks with pre-commit
+
+Add this to your `.pre-commit-config.yaml`
+
+```
+- repo: https://github.com/OpenTAI/pre-commit-hooks
+  rev: main  # Use the ref you want to point at
+  hooks:
+    - id: check-copyright
+      args: ["dir_to_check"]  # replace the dir_to_check with your expected directory to check
+```
+
+## Hooks available
+
+### say-hello
+
+A template to show how to implement a pre-commit hook
---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup  # type: ignore
+
+
+def readme():
+    with open('./README.md', encoding='utf-8') as f:
+        content = f.read()
+    return content
+
+
+setup(
+    name='pre_commit_hooks',
+    version='0.0.1',
+    description='A pre-commit hook for OpenTAI projects',
+    long_description=readme(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/OpenTAI/pre-commit-hooks',
+    author='OpenTAI Team',
+    author_email='',
+    packages=find_packages(),
+    python_requires='>=3.6',
+    install_requires=['PyYAML'],
+    entry_points={
+        'console_scripts': [
+            'say-hello=pre_commit_hooks.say_hello:main',
+        ],
+    },
+)
